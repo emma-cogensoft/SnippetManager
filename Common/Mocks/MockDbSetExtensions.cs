97f5aeadca4902c6c3c4902926b9cb2720632f9f
Emma Whitcroft <97703057+emma-cogensoft@users.noreply.github.com>
2022-07-11 13:35:48 +0100

    Move and fix application tests

--- Common/Mocks/MockDbSetExtensions.cs
+++ Common/Mocks/MockDbSetExtensions.cs
@@ -1,27 +1,120 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Moq;
 
 namespace Cogensoft.SnippetManager.Common.Mocks
 {
     public static class MockDbSetExtensions
     {
-        public static void SetUpDbSet<T>(this Mock<DbSet<T>> mock, IList<T> list)
+        public static void SetUpDbSet<T>(this Mock<DbSet<T>> dbSet, IList<T> sourceList)
             where T : class
         {
-            // var queryable = list.AsQueryable();
-            //
-            // mock.Setup(p => p.GetEnumerator())
-            //     .Returns(queryable.GetEnumerator());
-            //
-            // mock.Setup(p => p.Provider)
-            //     .Returns(queryable.Provider);
-            //
-            // mock.Setup(p => p.ElementType)
-            //     .Returns(queryable.ElementType);
-            //
-            // mock.Setup(p => p.Expression)
-            //     .Returns(queryable.Expression);
+            var queryable = sourceList.AsQueryable();
+
+            dbSet.As<IAsyncEnumerable<T>>()
+                .Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+                .Returns(new MockAsyncEnumerator<T>(queryable.GetEnumerator()));
+
+            dbSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(new MockAsyncQueryProvider<T>(queryable.Provider));
+
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>(sourceList.Add);
+        }
+
+        private class MockAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+        {
+            private readonly IQueryProvider _inner;
+
+            internal MockAsyncQueryProvider(IQueryProvider inner)
+            {
+                _inner = inner;
+            }
+
+            public IQueryable CreateQuery(Expression expression)
+            {
+                return new MockAsyncEnumerable<TEntity>(expression);
+            }
+
+            public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+            {
+                return new MockAsyncEnumerable<TElement>(expression);
+            }
+
+            public object Execute(Expression expression)
+            {
+                return _inner.Execute(expression);
+            }
+
+            public TResult Execute<TResult>(Expression expression)
+            {
+                return _inner.Execute<TResult>(expression);
+            }
+
+            public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+            {
+                var expectedResultType = typeof(TResult).GetGenericArguments()[0];
+                var executionResult = typeof(IQueryProvider)
+                    .GetMethod(name: nameof(IQueryProvider.Execute), genericParameterCount: 1,
+                        types: new[] { typeof(Expression) })
+                    .MakeGenericMethod(expectedResultType)
+                    .Invoke(this, new[] { expression });
+
+                return (TResult)typeof(Task)
+                    .GetMethod(nameof(Task.FromResult))
+                    ?.MakeGenericMethod(expectedResultType)
+                    .Invoke(null, new[] { executionResult });
+            }
+        }
+
+        private class MockAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+        {
+            IQueryProvider IQueryable.Provider => new MockAsyncQueryProvider<T>(this);
+
+            public MockAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable)
+            {
+            }
+
+            public MockAsyncEnumerable(Expression expression) : base(expression)
+            {
+            }
+
+            public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+            {
+                return new MockAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+            }
+        }
+
+        private class MockAsyncEnumerator<T> : IAsyncEnumerator<T>
+        {
+            private readonly IEnumerator<T> _inner;
+
+            public T Current => _inner.Current;
+
+            public MockAsyncEnumerator(IEnumerator<T> inner)
+            {
+                _inner = inner;
+            }
+
+            public ValueTask<bool> MoveNextAsync()
+            {
+                return new ValueTask<bool>(_inner.MoveNext());
+            }
+
+            public ValueTask DisposeAsync()
+            {
+                _inner.Dispose();
+
+                return new ValueTask();
+            }
         }
     }
 }
